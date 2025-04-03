c23a33d119ad225ed53df46ba2b2bf04da4d115d
Nevena Kicanovic <nevena@contactus.se>
2025-04-03 09:53:21 +0200

    NorthwindServiceServerSide

---
+++ PraktiskaAppar/Northwind.Blazor/Services/NorthwindServiceServerSide.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Northwind.Blazor.Services
+{
+    public class NorthwindServiceServerSide : INorthwindService
+    {
+        public Task<Customer> CreateCustomerAsync(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustomerAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer?> GetCustomerAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetCustomersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetCustomersAsync(string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateCustomerAsync(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
