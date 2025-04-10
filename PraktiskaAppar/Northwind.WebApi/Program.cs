a2ee336f942f925204e5e2777a6abb6ae3674e3f
Nevena Kicanovic <nevena@contactus.se>
2025-04-10 16:06:44 +0200

    HttpLogging

--- PraktiskaAppar/Northwind.WebApi/Program.cs
+++ PraktiskaAppar/Northwind.WebApi/Program.cs
@@ -3,8 +3,17 @@ using Northwind.EntityModels; //AddNorthwindContext
 using Microsoft.Extensions.Caching.Memory; // För att använda IMemoryCache
 using Northwind.WebApi.Repositories; // För att använda ICustomerRepository
 using Swashbuckle.AspNetCore.SwaggerUI; // För att anvädna SubmitMethod
+using Microsoft.AspNetCore.HttpLogging; // För att använda HttpLoggingMiddleware
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096; //standard är 32k
+    options.ResponseBodyLogLimit = 4096; //standard är 32k
+
+});
 
 builder.Services.AddSingleton<IMemoryCache>(
     new MemoryCache(new MemoryCacheOptions())); //Lägg till IMemoryCache
@@ -41,6 +50,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseHttpLogging(); //Lägg till HttpLoggingMiddleware
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
